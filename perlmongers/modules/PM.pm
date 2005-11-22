cbb064a4f575e1b4b147a8f0ecb67c1fb398d655
davorg <davorg@78cd1247-ddee-0310-a06a-93c6c5de3876>
2005-11-22 19:45:30 +0000

    Added hostname. Moved connection parameters to env vars.

--- perlmongers/modules/PM.pm
+++ perlmongers/modules/PM.pm
@@ -5,8 +5,22 @@ package PM;
 
 use Class::DBI::Loader;
 
-my $loader = Class::DBI::Loader->new( dsn       => 'dbi:mysql:pm_census',
-                                      user      => 'pm_census',
-                                      password  => 'pm_c3n5u5',
+my @missing;
+foreach (qw(PM_USER PM_PASS)) {
+  push @missing, $_ unless defined $ENV{$_};
+}
+
+if (@missing) {
+  die 'You must set the ' . join(' and ', @missing) .
+    " enviroment variables.\n";
+}
+
+my $db  = $ENV{PM_DB} || 'pm_census';
+my $dsn = "dbi:mysql:database=$db";
+$dsn .= ";host=$ENV{PM_HOST}" if $ENV{PM_HOST};
+
+my $loader = Class::DBI::Loader->new( dsn       => $dsn,
+                                      user      => $ENV{PM_USER},
+                                      password  => $ENV{PM_PASS},
                                       namespace => 'PM' );
 
