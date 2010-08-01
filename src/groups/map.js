e2c7cb9de3fb046d0ebe0e5b6d0bfe178edf39e2
gabor <gabor@78cd1247-ddee-0310-a06a-93c6c5de3876>
2010-08-01 09:14:35 +0000

    indentation

--- src/groups/map.js
+++ src/groups/map.js
@@ -9,68 +9,66 @@ var sa = new GPoint(-63.63, -25.00);
 
 
 
-    var map;
-    function onLoad() {
-      // Center the map
-      map = new GMap(document.getElementById("map"));
-      map.addControl(new GLargeMapControl());
-      map.addControl(new GMapTypeControl());
-      //map.centerAndZoom(new GPoint(-78.6588, 35.8219), 16);
-      map.centerAndZoom(new GPoint(0.0, 18.0), 15);
-      var spamicon = new GIcon();
-      spamicon.image = "http://labs.google.com/ridefinder/images/mm_20_red.png";
-      spamicon.shadow = "http://labs.google.com/ridefinder/images/mm_20_shadow.png";
-      spamicon.iconSize = new GSize(12, 20);
-      spamicon.shadowSize = new GSize(22, 20);
-      spamicon.iconAnchor = new GPoint(6, 20);
-      spamicon.infoWindowAnchor = new GPoint(5, 1);
+var map;
+function onLoad() {
+  // Center the map
+  map = new GMap(document.getElementById("map"));
+  map.addControl(new GLargeMapControl());
+  map.addControl(new GMapTypeControl());
+  //map.centerAndZoom(new GPoint(-78.6588, 35.8219), 16);
+  map.centerAndZoom(new GPoint(0.0, 18.0), 15);
+  var spamicon = new GIcon();
+  spamicon.image = "http://labs.google.com/ridefinder/images/mm_20_red.png";
+  spamicon.shadow = "http://labs.google.com/ridefinder/images/mm_20_shadow.png";
+  spamicon.iconSize = new GSize(12, 20);
+  spamicon.shadowSize = new GSize(22, 20);
+  spamicon.iconAnchor = new GPoint(6, 20);
+  spamicon.infoWindowAnchor = new GPoint(5, 1);
 
-        // Creates a marker whose info window displays the given number
-            function createMarkerJay(point, spamicon, name, web, lat, lng) {
-              var marker = new GMarker(point, spamicon);
+    // Creates a marker whose info window displays the given number
+        function createMarkerJay(point, spamicon, name, web, lat, lng) {
+          var marker = new GMarker(point, spamicon);
 
-              // Show this marker's index in the info window when it is clicked
-              var msg = "<small>";
-              msg = msg+"<a href='" + web + "'>" + name + "</a><br/>";
-              msg = msg+"<nobr>" + lat + ", " + lng + "</nobr>";
-              msg = msg+"</small>";
+          // Show this marker's index in the info window when it is clicked
+          var msg = "<small>";
+          msg = msg+"<a href='" + web + "'>" + name + "</a><br/>";
+          msg = msg+"<nobr>" + lat + ", " + lng + "</nobr>";
+          msg = msg+"</small>";
 
-              GEvent.addListener(marker, "click", function() {
-                marker.openInfoWindowHtml(msg);
-              });
+          GEvent.addListener(marker, "click", function() {
+            marker.openInfoWindowHtml(msg);
+          });
 
-              return marker;
-            }
-
-      // Download the data in map.xml and load it on the map.
-      var request = GXmlHttp.create();
-      request.open("GET", "map.xml", true);
-      request.onreadystatechange = function() {
-        if (request.readyState == 4) {
-          var xmlDoc = request.responseXML;
-          var markers = xmlDoc.documentElement.getElementsByTagName("marker");
-          for (var i = 0; i < markers.length; i++) {
-            var point = new GPoint(parseFloat(markers[i].getAttribute("lng")),
-                                   parseFloat(markers[i].getAttribute("lat")));
-            //var marker = new GMarker(point);
-              var marker = createMarkerJay(point, spamicon,
-                    markers[i].getAttribute("name"), markers[i].getAttribute("web"),
-                    markers[i].getAttribute("lat"),  markers[i].getAttribute("lng"));
-              map.addOverlay(marker);
-          }
+          return marker;
         }
-      }
-      request.send(null);
-    }
 
-    function cz(p, z) {
-        map.centerAndZoom(p, z);
+  // Download the data in map.xml and load it on the map.
+  var request = GXmlHttp.create();
+  request.open("GET", "map.xml", true);
+  request.onreadystatechange = function() {
+    if (request.readyState == 4) {
+      var xmlDoc = request.responseXML;
+      var markers = xmlDoc.documentElement.getElementsByTagName("marker");
+      for (var i = 0; i < markers.length; i++) {
+        var point = new GPoint(parseFloat(markers[i].getAttribute("lng")),
+                               parseFloat(markers[i].getAttribute("lat")));
+        //var marker = new GMarker(point);
+          var marker = createMarkerJay(point, spamicon,
+                markers[i].getAttribute("name"), markers[i].getAttribute("web"),
+                markers[i].getAttribute("lat"),  markers[i].getAttribute("lng"));
+          map.addOverlay(marker);
+      }
     }
+  }
+  request.send(null);
+}
+
+function cz(p, z) {
+    map.centerAndZoom(p, z);
+}
 
 
 function initialize() {
-  //alert(longitude['min'] + " " + longitude['max']);
-  //alert(latitude['min']  + " " + latitude['max']);
 
   if (GBrowserIsCompatible()) {
     var map = new GMap2(document.getElementById("map-canvas"));
