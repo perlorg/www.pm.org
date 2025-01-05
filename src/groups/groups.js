bd910e8663640b6d8e93604cfdbaa54e371bffe9
Jay Hannah <jay.hannah@iinteractive.com>
2025-01-05 03:54:30 -0600

    Pretty good starter groups/index.html map?

--- src/groups/groups.js
+++ src/groups/groups.js
@@ -3,79 +3,21 @@
 var pmGroups = {
 	"type": "FeatureCollection",
 	"features": [
-		{
-			"geometry": {
-				"type": "Point",
-				"coordinates": [-95.934398, 41.257502]
-			},
-			"type": "Feature",
-			"properties": {
-				"popupContent": "Omaha: Food distribution every Sunday at 2pm. Southeast corner of 14th and Farnam.",
-				"website": "http://omaha.foodnotbombs.us",
-			},
-			"id": 1
-		},
-		{
-			"geometry": {
-				"type": "Point",
-				"coordinates": [-75.46321855218785, 40.61286116502885]
-			},
-			"type": "Feature",
-			"properties": {
-				"popupContent": "Lehigh Valley, PA: Food distribution every Saturday, 3:00 standard time (Nov-Mar), 4:30 daylight saving time (Mar-Nov). In front of Sheridan Elementary, corner of 2nd and Liberty in Allentown.",
-				"instagram": "https://www.instagram.com/lvfoodnotbombs/",
-			},
-			"id": 2
-		},
-		{
-			"geometry": {
-				"type": "Point",
-				"coordinates": [-81.0991178466344, 32.058268269850444]
-			},
-			"type": "Feature",
-			"properties": {
-				"popupContent": "Savannah GA: Sundays at 1 pm at Thomas Square Park, which is directly next to the Bull street library.",
-				"facebook": "https://www.facebook.com/SavannahFNB",
-			},
-			"id": 3
-		},
-		{
-			"geometry": {
-				"type": "Point",
-				"coordinates": [-95.36883085501599, 29.75927985426768]
-			},
-			"type": "Feature",
-			"properties": {
-				"popupContent": "Houston TX: Hungry? Want to Help? Come to 550 McKinney St, Houston, TX 77002 Mon., Wed., Fri, & Sun. at 7pm and help out.  Bring vegan food or just bring yourself! We have bowls & spoons.",
-				"website": "https://houstonfoodnotbombs.org/",
-			},
-			"id": 4
-		},
-		{
-			"geometry": {
-				"type": "Point",
-				"coordinates": [-120.65793978200787, 35.278731159930985]
-			},
-			"type": "Feature",
-			"properties": {
-				"popupContent": "San Luis Obispo, CA: Food distribution every Sunday, 1:00 - 3:00 pm Pacific time (rain or shine), In gazebo at Mitchell Park, block of Osos, Pismo, Buchon and Santa Rosa Streets.",
-				"instagram": "https://www.instagram.com/foodnotbombs_slo",
-				"facebook": "https://www.facebook.com/FoodNotBombsSLO",
-			},
-			"id": 5
-		},
-		{
-			"geometry": {
-				"type": "Point",
-				"coordinates": [101.698038, 3.148108]
-			},
-			"type": "Feature",
-			"properties": {
-				"popupContent": "Kuala Lumpur, Malaysia: serving free vegan or vegetarian meal to the masses, particularly the homeless persons.",
-				"instagram": "https://www.instagram.com/foodnotbombskl/",
-				"facebook": "https://www.facebook.com/profile.php?id=100064666542950",
-			},
-			"id": 6
-		},
+    [% FOREACH group IN allgroups; group=group.value %]
+      [% IF group.latitude AND group.longitude %]
+        {
+          "geometry": {
+            "type": "Point",
+            "coordinates": [[% group.longitude %], [% group.latitude %]]
+          },
+          "type": "Feature",
+          "properties": {
+            "popupContent": "[% group.name | html_entity %]",
+            "website": "[% group.web %]",
+          },
+          "id": [% group.id %]  
+        },
+      [% END %]
+    [% END %]
 	]
 };
